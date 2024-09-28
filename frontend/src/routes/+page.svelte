37e34087a500dbef6db0fbc0a3923a9aebc4e1c9
luuklin <37289522+luuklin@users.noreply.github.com>
2024-09-28 23:05:31 +0200

    My changes
    Should just be visual/layout changes if I remember correctly. Been a while since I looked at this.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -43,7 +43,20 @@
   let intensityValue = 255;
   let durationValue = 255;
   let actionZap = true;
+  let settingsShown = false;
 
+  function swapPage(){
+    settingsShown = !settingsShown;
+    let settingsDiv = document.getElementById("SettingsDiv");
+    let shockDiv = document.getElementById("ShockDiv");
+    if (settingsShown){
+      shockDiv.style["display"] = "none"
+      settingsDiv.style["display"] = "inline"
+    } else {
+      shockDiv.style["display"] = "inline"
+      settingsDiv.style["display"] = "none"
+    }
+  }
   function performAction() {
     const data = SerializeActionCommand(actionZap ? 1 : 2, 255 - intensityValue, 255 - durationValue);
     WebSocketClient.Instance.Send(data);
@@ -67,74 +80,108 @@
   }
 </script>
 
-<div class="flex flex-col items-center justify-center h-full">
-  <div class="flex-col space-y-5 w-full max-w-md">
-    <WiFiList />
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 2fr;
+    grid-template-rows: 3fr 1fr 1fr 1fr;
+    grid-column-gap: 2%;
+    grid-row-gap: 2%;
+    height: 90vh;
+    width: 90vw;
+    color: black;
+    font-size: 2em;
 
-    <div class="flex flex-col space-y-2">
-      <h3 class="h3">Account Linking</h3>
-      <div class="flex space-x-2">
-        <input class={'input variant-form-material ' + (linkCodeValid ? '' : 'input-error')} type="text" inputmode="numeric" pattern="[0-9]*" placeholder="Link Code" bind:value={linkCode} />
-        <button class="btn variant-filled" on:click={linkAccount} disabled={!linkCodeValid || linkCode.length < 6}>Link</button>
-      </div>
-    </div>
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
 
-    <div class="flex flex-col space-y-2">
-      <div class="flex flex-row space-x-2 items-center">
-        <h3 class="h3">RF TX Pin</h3>
-        <span class="text-sm text-gray-500">(Currently {$DeviceStateStore.config == null ? ' not set' : $DeviceStateStore.config.rf.txPin}) </span>
-      </div>
-      <div class="flex space-x-2">
-        <input class="input variant-form-material" type="number" placeholder="TX Pin" bind:value={rfTxPin} />
-        <button class="btn variant-filled" on:click={setRfTxPin} disabled={!rfTxPinValid}>Set</button>
-      </div>
-    </div>
+    touch-action: none;
+  }
 
-    <style>
-      .container {
-        display: grid;
-        grid-template-columns: 1fr 1fr 2fr;
-        grid-template-rows: 3fr 1fr 1fr 1fr;
-        grid-column-gap: 5px;
-        grid-row-gap: 5px;
-        height: 80vh;
-        width: 100vw;
-      }
+  .btn {
+    border-radius: 0;
+    background-color: #FFEE24;
 
-      .slider {
-        align-self: center;
-        justify-self: center;
-        writing-mode: vertical-lr;
-        height: 100%;
-      }
-      .spanText {
-        display: flex;
-        justify-content: center;
-      }
-    </style>
+  }
 
-    <div class="container">
-      <input id="intensitySlider" type="range" min="0" max="255" class="slider" style="grid-area: 1 / 1 / 4 / 2" bind:value={intensityValue} />
-      <input id="durationSlider" type="range" min="0" max="255" class="slider" style="grid-area: 1 / 2 / 4 / 3" bind:value={durationValue} />
-      <span id="intensityText" class="spanText" style="grid-area: 4 / 1 / 5 / 2">{(((255 - intensityValue) / 255.0) * 100.0).toFixed(1) + '%'}</span>
-      <span id="durationText" class="spanText" style="grid-area: 4 / 2 / 5 / 3">{(((255 - durationValue) / 255) * 15).toFixed(1) + 's'}</span>
-      <button id="performActionButton" class="btn variant-filled" style="grid-area: 1 / 3 / 2 / 4" on:click={performAction}>{actionZap ? 'Zap' : 'Vibrate'}</button>
-      <button id="switchModeButton" class="btn variant-filled" style="grid-area: 2 / 3 / 3 / 4" on:click={switchMode}>Switch Mode</button>
-      <button id="beepButton" class="btn variant-filled" style="grid-area: 3 / 3 / 4 / 4" on:click={performBeep}>Beep</button>
-      <button id="stopButton" class="btn variant-filled" style="grid-area: 4 / 3 / 5 / 4" on:click={performStop}>Stop</button>
-    </div>
-    <!-- <div class="items-center">
+  .slider {
+    appearance: none;
+    writing-mode: vertical-lr;
+    background-color: black;
+    margin: auto;
+    width: 70%;
+    height: 100%;
+    outline-color: #FFEE24;
+    outline-style: solid;
+    outline-width: 1%;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    background-color: #FFEE24;
+    width: 23vw;
+    height: 23vw;
+    cursor: pointer;
+  }
+
+  .slider::-moz-range-thumb {
+    appearance: none;
+    background-color: #FFEE24;
+    width: 23vw;
+    height: 23vw;
+    cursor: pointer;
+  }
+
+  .spanText {
+    display: flex;
+    justify-content: center;
+    color: #FFEE24;
+    margin: auto;
+  }
+</style>
+
+<div class="shockPage" style="height: 100%; overflow: hidden;">
+  <div>
+
+    <div id="SettingsDiv" style="display: none">
+      <WiFiList />
+
       <div class="flex flex-col space-y-2">
-        <input type="range" class="slider" style="writing-mode: vertical-lr" />
-        <input type="range" class="slider" style="writing-mode: vertical-lr" />
-        <input type="range" class="slider" style="writing-mode: vertical-lr" />
+        <h3 class="h3">Account Linking</h3>
+        <div class="flex space-x-2">
+          <input class={'input variant-form-material ' + (linkCodeValid ? '' : 'input-error')} type="text" inputmode="numeric" pattern="[0-9]*" placeholder="Link Code" bind:value={linkCode} />
+          <button class="btn variant-filled" on:click={linkAccount} disabled={!linkCodeValid || linkCode.length < 6}>Link</button>
+        </div>
       </div>
 
       <div class="flex flex-col space-y-2">
-        <button class="btn">Button 1</button>
-        <button class="btn">Button 2</button>
-        <button class="btn">Button 3</button>
+        <div class="flex flex-row space-x-2 items-center">
+          <h3 class="h3">RF TX Pin</h3>
+          <span class="text-sm text-gray-500">(Currently {$DeviceStateStore.config == null ? ' not set' : $DeviceStateStore.config.rf.txPin}) </span>
+        </div>
+        <div class="flex space-x-2">
+          <input class="input variant-form-material" type="number" placeholder="TX Pin" bind:value={rfTxPin} />
+          <button class="shockButton" on:click={setRfTxPin} disabled={!rfTxPinValid}>Set</button>
+        </div>
       </div>
-    </div> -->
+    </div>
+
+    <div id="ShockDiv">
+      <div class="container">
+        <input id="intensitySlider" type="range" min="0" max="255" class="slider" style="grid-area: 1 / 1 / 4 / 2" bind:value={intensityValue} />
+        <input id="durationSlider" type="range" min="0" max="255" class="slider" style="grid-area: 1 / 2 / 4 / 3" bind:value={durationValue} />
+        <span id="intensityText" class="spanText" style="grid-area: 4 / 1 / 5 / 2">{(((255 - intensityValue) / 255.0) * 100.0).toFixed(0) + '%'}</span>
+        <span id="durationText" class="spanText" style="grid-area: 4 / 2 / 5 / 3">{(((255 - durationValue) / 255) * 15).toFixed(1) + 's'}</span>
+        
+        <button id="performActionButton" class="btn" style="grid-area: 1 / 3 / 2 / 4" on:click={performAction}>{actionZap ? 'Zap' : 'Vibrate'}</button>
+        <button id="switchModeButton" class="btn" style="grid-area: 2 / 3 / 3 / 4" on:click={switchMode}>Switch Mode</button>
+        <button id="beepButton" class="btn" style="grid-area: 3 / 3 / 4 / 4" on:click={performBeep}>Beep</button>
+        <button id="stopButton" class="btn" style="grid-area: 4 / 3 / 5 / 4" on:click={performStop}>Stop</button>
+      </div>
+    </div>
+
   </div>
 </div>
